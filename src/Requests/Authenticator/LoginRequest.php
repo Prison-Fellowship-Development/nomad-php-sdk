a3b491944254604c0e24c6ca7e34a92ceae130b9
guerrerocing <guerrerocing@outlook.com>
2024-08-29 00:41:11 -0600

    fix: Refactorization, HasBody implementation added and use HasJsonBody Trait
    - LoginRequest.php
    - LogoutRequest.php
    - RefreshToken.php

--- src/Requests/Authenticator/LoginRequest.php
+++ src/Requests/Authenticator/LoginRequest.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace PrisonFellowship\NomadPHPSDK\Requests\Authenticator;
 
+use Saloon\Contracts\Body\HasBody;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
+use Saloon\Traits\Body\HasJsonBody;
 
-class LoginRequest extends Request
+class LoginRequest extends Request implements HasBody
 {
+    use HasJsonBody;
+
     protected Method $method = Method::POST;
 
     public function __construct(
--- src/Requests/Authenticator/LogoutRequest.php
+++ src/Requests/Authenticator/LogoutRequest.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace PrisonFellowship\NomadPHPSDK\Requests\Authenticator;
 
+use Saloon\Contracts\Body\HasBody;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
+use Saloon\Traits\Body\HasJsonBody;
 
-class LogoutRequest extends Request
+class LogoutRequest extends Request implements Hasbody
 {
+    use HasJsonBody;
+
     protected Method $method = Method::POST;
 
     public function __construct(protected string $token)
