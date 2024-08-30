45b5f185c83e3725d2585c71f7fa28548f9ea0fd
guerrerocing <guerrerocing@outlook.com>
2024-08-30 16:06:12 -0600

    fix: Refactorization in Utility Requests, HasBody implementation added and use HasJsonBody Trait

--- src/Requests/Utility/ForgotPasswordRequest.php
+++ src/Requests/Utility/ForgotPasswordRequest.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace PrisonFellowship\NomadPHPSDK\Requests\Utility;
 
+use Saloon\Contracts\Body\HasBody;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
+use Saloon\Traits\Body\HasJsonBody;
 
-class ForgotPasswordRequest extends Request
+class ForgotPasswordRequest extends Request implements Hasbody
 {
+    use HasJsonBody;
+
     protected Method $method = Method::POST;
 
     public function __construct(protected string $username)
--- src/Requests/Utility/ResetPasswordRequest.php
+++ src/Requests/Utility/ResetPasswordRequest.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace PrisonFellowship\NomadPHPSDK\Requests\Utility;
 
+use Saloon\Contracts\Body\HasBody;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
+use Saloon\Traits\Body\HasJsonBody;
 
-class ResetPasswordRequest extends Request
+class ResetPasswordRequest extends Request implements Hasbody
 {
+    use HasJsonBody;
+
     protected Method $method = Method::POST;
 
     public function __construct(
