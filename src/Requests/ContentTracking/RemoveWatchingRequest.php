eb08b9c595ccb780d6405a04bcdfcab10db5e7ad
guerrerocing <guerrerocing@outlook.com>
2025-03-31 09:06:11 -0600

    Add RemoveWatchingRequest to handle removing watching content

---
+++ src/Requests/ContentTracking/RemoveWatchingRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PrisonFellowship\NomadPHPSDK\Requests\ContentTracking;
+
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Http\Request;
+use Saloon\Enums\Method;
+use Saloon\Traits\Body\HasJsonBody;
+
+class RemoveWatchingRequest extends Request implements HasBody
+{
+
+    use HasJsonBody;
+    protected Method $method = Method::POST;
+
+    public function __construct(
+        protected string $token,
+        protected string $assetId,
+        protected string $profileId,
+    ) {}
+
+    protected function defaultHeaders(): array
+    {
+        return [
+            'Authorization' => 'Bearer '.$this->token,
+            'Content-Type' => 'application/json',
+        ];
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return "/api/portal/remove-watching/{$this->assetId}?profileId={$this->profileId}";
+    }
+
+}
