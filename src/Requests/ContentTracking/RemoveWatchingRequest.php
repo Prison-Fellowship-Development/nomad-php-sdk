611bde2845c3086a261d36d07d4b8b11f76ecd1e
guerrerocing <guerrerocing@outlook.com>
2025-05-22 23:43:01 -0600

    style: format RemoveWatchingRequest for readability

--- src/Requests/ContentTracking/RemoveWatchingRequest.php
+++ src/Requests/ContentTracking/RemoveWatchingRequest.php
@@ -5,21 +5,22 @@ declare(strict_types=1);
 namespace PrisonFellowship\NomadPHPSDK\Requests\ContentTracking;
 
 use Saloon\Contracts\Body\HasBody;
-use Saloon\Http\Request;
 use Saloon\Enums\Method;
+use Saloon\Http\Request;
 use Saloon\Traits\Body\HasJsonBody;
 
 class RemoveWatchingRequest extends Request implements HasBody
 {
-
     use HasJsonBody;
+
     protected Method $method = Method::POST;
 
     public function __construct(
         protected string $token,
         protected string $assetId,
         protected string $profileId,
-    ) {}
+    ) {
+    }
 
     protected function defaultHeaders(): array
     {
@@ -33,5 +34,4 @@ class RemoveWatchingRequest extends Request implements HasBody
     {
         return "/api/portal/remove-watching/{$this->assetId}?profileId={$this->profileId}";
     }
-
 }
